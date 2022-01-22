ae2e6c3db3e9867df1144e6e878ec2fc2dedf17b
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2022-01-22 19:49:32 +0100

    this fixes the postgres connection issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,10 @@ export const Config = () => ({
     jwtSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     database: {
         type: process.env.DB_DRIVER || "mysql",
+        url: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+          },
         host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || 3306,
         username: process.env.DB_USERNAME || "root",
@@ -13,6 +17,7 @@ export const Config = () => ({
         entities: [
             "dist/**/entities/*.entity.js",
             // User
-        ]
+        ],
+        autoLoadEntites: true
     }
 });
